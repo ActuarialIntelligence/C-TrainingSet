0aa0f97d56315f949be4b52b8da4b110fc0c8d25
Citizen <codeaiplings@gmail.com>
2022-11-26 20:12:55 +0200

    small fix to last commit

--- AI.Infrastructure.Data/Dtos/FileHelperDto.cs
+++ AI.Infrastructure.Data/Dtos/FileHelperDto.cs
@@ -5,7 +5,7 @@ namespace AI.Infrastructure.Data.Dtos
 
         [DelimitedRecord(",")] // Tells us the delimiter
         [IgnoreFirst]
-        public class StringArrayDto // defines the return object with all of the rows that will be populated
+        public class FileHelperDto // defines the return object with all of the rows that will be populated
         {
             public string[] Row;
         }
