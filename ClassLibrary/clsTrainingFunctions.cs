0efc7d64ab64d8877d9360f55a982d0cd23e5cd4
Citizen <codeaiplings@gmail.com>
2022-11-26 17:29:09 +0200

    // in this first we are going to gain a feel for the IDE and how to make efficient use of it.
    //NuGet
    //Referencing
    //Project Creation and Naming conventions for labelling items
    // Building and Deploying

--- ClassLibrary/clsTrainingFunctions.cs
+++ ClassLibrary/clsTrainingFunctions.cs
@@ -1,8 +1,24 @@
-﻿using System;
+﻿using System; // these are a list of references that this library will make use of.
 
-namespace ClassLibrary
+namespace ClassLibrary // This is called a namespace and one can call functions within a namespace without referencing.
+    // Referencing another project (for now functions within other namespaces) is required to access functions within another namespace
+    //its a logical grouping
 {
-    public class clsTrainingFunctions
+    //the following is a class object: clsTrainingFunctions
+    public class clsTrainingFunctions // There are four initial [access modifiers] that you need to be aware of
+        //public| any calling object with a reference can access
+        //private| only other members within the class have access
+        //abstract| deals with inheretance (we will get to that)
+        //internal| again we wil get to this later.
     {
+        // we put function methods in here
+        // again these can be access controlled with the same modifiers
     }
 }
+
+
+// in this first we are going to gain a feel for the IDE and how to make efficient use of it.
+//NuGet
+//Referencing
+//Project Creation and Naming conventions for labelling items
+// Building and Deploying 
