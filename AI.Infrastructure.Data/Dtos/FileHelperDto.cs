b8fff1d51453ccd891b05253dd47263a6e28396f
Citizen <codeaiplings@gmail.com>
2022-11-26 20:12:27 +0200

    Introducing FileHelper Dto's for use with readers.

---
+++ AI.Infrastructure.Data/Dtos/FileHelperDto.cs
@@ -0,0 +1,13 @@
+﻿
+    using FileHelpers;
+namespace AI.Infrastructure.Data.Dtos
+{
+
+        [DelimitedRecord(",")] // Tells us the delimiter
+        [IgnoreFirst]
+        public class StringArrayDto // defines the return object with all of the rows that will be populated
+        {
+            public string[] Row;
+        }
+    
+}
