2b0608c441fe740b9809998ac1baecea79f4c753
Citizen <codeaiplings@gmail.com>
2022-11-28 16:17:04 +0200

    Section covers Domain Objects and building up to an API : BondDetails

---
+++ AI.Domain/ContainerObjects/BondDetails.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AI.Domain.ContainerObjects
+{
+    public class BondDetails
+    {
+        public decimal nominal { get; private set; }
+        public decimal yearlyCouponRate { get; private set; }
+        public decimal redemptionRate { get; private set; }
+        public decimal term { get; private set; }
+        public decimal inflation { get; private set; }
+
+        public BondDetails(decimal nominal,
+            decimal yearlyCouponRate,
+            decimal redemptionRate,
+            decimal term,
+            decimal inflation)
+        {
+            this.nominal = nominal;
+            this.yearlyCouponRate = yearlyCouponRate;
+            this.redemptionRate = redemptionRate;
+            this.term = term;
+            this.inflation = inflation;
+        }
+
+    }
+}
