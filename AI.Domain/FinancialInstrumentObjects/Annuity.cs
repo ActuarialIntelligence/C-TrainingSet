fad263ea5dd1c03b572270b93e391c6d3fa13dc5
Citizen <codeaiplings@gmail.com>
2022-11-28 16:44:56 +0200

    This Section Covers Details of Domain Objects: AnnuityObject

---
+++ AI.Domain/FinancialInstrumentObjects/Annuity.cs
@@ -0,0 +1,49 @@
+﻿using AI.Domain.ContainerObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AI.Domain.FinancialInstrumentObjects
+{
+    public class Annuity
+    {
+        private readonly ListTermCashflowSet cashFlowSet;
+        private int days;
+        public Annuity(ListTermCashflowSet cashFlowSet, int days)
+        {
+            this.cashFlowSet = cashFlowSet;
+            this.days = days;
+        }
+
+        public decimal GetZSpreadPV(decimal zSpread)
+        {
+            var total = 0m;
+            DateTime anchorDate = cashFlowSet.AnchorDate;
+            foreach (var cashFlow in cashFlowSet.CashflowSet)
+            {
+                total += cashFlow.cashflow * discountFactor(cashFlow.spotYield.Yield + zSpread,
+                    cashFlowSet.DifferenceInUnitsBetweenDates(anchorDate, cashFlow.date, days));
+            }
+            return total;
+        }
+
+        public decimal GetPV()
+        {
+            var total = 0m;
+            DateTime anchorDate = cashFlowSet.AnchorDate;
+            foreach (var cashFlow in cashFlowSet.CashflowSet)
+            {
+                total += cashFlow.cashflow * discountFactor(cashFlow.spotYield.Yield,
+                    cashFlowSet.DifferenceInUnitsBetweenDates(anchorDate, cashFlow.date, days));
+            }
+            return total;
+        }
+
+        private decimal discountFactor(decimal yield, decimal term)
+        {
+            var d = (1 / (1 + yield));
+            var dp = Math.Pow((double)d, (double)term);
+            return (decimal)dp;
+        }
+    }
+}
