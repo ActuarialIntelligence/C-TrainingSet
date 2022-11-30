3b73c0df4591762fb195f5f600b50780e9ffbc77
Citizen <codeaiplings@gmail.com>
2022-12-01 01:03:53 +0200

    Section Covers: First Service : ZSpread Web API Compatible with Docker.

---
+++ AI.Web.ZSpreadService/Controllers/ZSpreadController.cs
@@ -0,0 +1,51 @@
+﻿using AI.Domain.ContainerObjects;
+using AI.Domain.FinancialInstrumentObjects;
+using AI.Domain.Mathematical_Technique_Objects;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AI.Web.ZSpreadService.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ZSpreadController : ControllerBase
+    {
+
+        // GET api/values
+        [HttpGet("Z-SpreadPresentValue")]
+        public ActionResult<decimal>
+            ZSpreadPresentValue(ListTermCashflowSet cashFlowSet, int days, decimal nominal)
+        {
+            Annuity annuity = new Annuity(cashFlowSet, days);
+            var result = Interpolation.Interpolate(annuity.GetZSpreadPV, 0.01m, 0.09m, nominal);
+            return result;
+        }
+
+        // GET api/values
+        [HttpGet("AnnuityPresentValue")]
+        public ActionResult<decimal>
+            AnnuityPresentValue(PlainListTermCashFlowSet cashFlowSet, int days, decimal nominal)
+        {
+            var cashFlowList =
+                new ListTermCashflowSet(cashFlowSet.cashflowSet, cashFlowSet.termType);
+            Annuity annuity = new Annuity(cashFlowList, days);
+
+            var result = annuity.GetPV();
+            return result;
+        }
+        /// <summary>
+        /// POST
+        /// </summary>
+        /// <param name="cashFlowSet"></param>
+        /// <returns></returns>
+        [HttpPost("Z-Spread")]
+        public ActionResult<decimal> ZSpread(PlainListTermCashFlowSet cashFlowSet)
+        {
+            var cashFlowList =
+    new ListTermCashflowSet(cashFlowSet.cashflowSet, cashFlowSet.termType);
+
+            var res = new ZSpread(cashFlowList, cashFlowSet.nominal);
+            var result = res.CalculateZspread();
+            return result;
+        }
+    }
+}
