165beded017827478e79faacd716a6796a672dab
Rajah <rajiyer@microsoft.com>
2022-12-24 01:02:29 +0200

    Introduced RouteObject

---
+++ AI.Domain/ContainerObjects/RouteObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AI.Domain.ContainerObjects
+{
+    public class RouteObject
+    {
+        public int id { get; private set; }
+        public string discription { get; private set; }
+        public string iconUrl { get; private set; }
+        public string technicalDetails { get; private set; }
+        public RouteObject(int id, string discription,
+            string iconUrl, string technicalDetails)
+        {
+            this.id = id;
+            this.iconUrl = iconUrl;
+            this.technicalDetails = technicalDetails;
+            this.discription = discription;
+        }
+    }
+}
